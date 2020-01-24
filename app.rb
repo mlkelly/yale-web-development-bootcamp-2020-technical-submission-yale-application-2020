2d52b575f9a645be44fadd5de724d9ba677dd48b
Maura Kelly <maurak87@gmail.com>
2020-01-24 23:02:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ age = "20"
 favorite_food = "Tamales"
 height_in_inches = "62"
 height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than aver." : "You are not taller than average."
+learn test 
 
 
 ## Do not modify below this line!
