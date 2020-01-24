75ab394671addb8b25d933b9a766fa281c50ce9d
Maura Kelly <maurak87@gmail.com>
2020-01-24 23:10:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Maura Kelly"
+age = "20"
+favorite_food = "Tamales"
+height_in_inches = "62"
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES? "You are taller than average." : "You are not taller than average."
+
 
 ## Do not modify below this line!
 
